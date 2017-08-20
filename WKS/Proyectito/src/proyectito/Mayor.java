39f2bab257ff87a2604211b2d2bfafb6610cfbf6
BxNxPxC <poncec.briann@gmail.com>
2017-08-20 11:34:00 -0300

    Clase Mayor
    Comenzando nuevo proyecto en java

---
+++ WKS/Proyectito/src/proyectito/Mayor.java
@@ -0,0 +1,15 @@
+package proyectito;
+
+public class Mayor {
+	
+	private int primero;
+	private int segundo;
+	private int tercero;
+	
+	public Mayor(int primero, int segundo, int tercero){
+		this.primero = primero;
+		this.segundo = segundo;
+		this.tercero = tercero;
+	}
+
+}
